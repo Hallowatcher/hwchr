47eeccb4b11ca070cddc9806595b423d9b5ca93b
Cubas Wiemer, Uwe <Uwe.Cubas-Wiemer@tecalliance.net>
2016-09-12 14:14:08 +0200

    Changed project layout

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 ﻿const express = require("express");
 const app = express();
+const stylus = require("stylus");
+const nib = require("nib");
+
+function compile(str, path) {
+    return stylus(str)
+        .set("filename", path)
+        .use(nib());
+}
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "jade");
+app.use(stylus.middleware({
+    src: __dirname + "/public",
+    compile: compile
+}));
 app.use(express.static(__dirname + "/public"));
 
 app.get("/",
