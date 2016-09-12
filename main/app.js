126ddb12ecab43a005d29bd1b086868f816c7d70
Cubas Wiemer, Uwe <Uwe.Cubas-Wiemer@tecalliance.net>
2016-09-12 13:40:02 +0200

    Integrated Jade

--- main/app.js
+++ main/app.js
@@ -1,9 +1,13 @@
 ﻿const express = require("express");
 const app = express();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
+app.use(express.static(__dirname + "/public"));
+
 app.get("/",
     function (req, res) {
-        res.send("works");
+        res.render("index");
     });
 
 app.listen(3000,
