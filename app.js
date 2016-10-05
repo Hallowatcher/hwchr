115108c78277c68254273ff54a5764d0b5359564
Cubas Wiemer, Uwe <Uwe.Cubas-Wiemer@tecalliance.net>
2016-10-05 09:03:40 +0200

    Changed route layout

--- app.js
+++ app.js
@@ -7,22 +7,18 @@ app.set("view engine", "pug");
 app.use(lessMiddleware(__dirname + "/public"));
 app.use(express.static(__dirname + "/public"));
 
-app.get("/",
-    function (req, res) {
-        res.render("hwchr/home", { page: "home" });
-    });
+app.get("/", function (req, res) {
+    res.render("hwchr/home", { page: "home" });
+});
 
-app.get("/about",
-    function (req, res) {
-        res.render("hwchr/about", { page: "about" });
-    });
+app.get("/about", function (req, res) {
+    res.render("hwchr/about", { page: "about" });
+});
 
-app.get("/projects",
-    function (req, res) {
-        res.render("hwchr/projects", { page: "projects" });
-    });
+app.get("/projects", function (req, res) {
+    res.render("hwchr/projects", { page: "projects" });
+});
 
-app.listen(3000,
-    function () {
-        console.log("Express listening on port 3000.");
-    });
+app.listen(3000, function () {
+    console.log("Express listening on port 3000.");
+});
